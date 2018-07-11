da2eb6a68a93137e2cb84f71c0950904ec9e83b8
enPiT-P31 <d802@oshima-k.ac.jp>
2018-07-11 16:57:28 +0900

    5章のじゃんけんプログラム

--- app/src/main/java/com/example/enpit_p12/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/enpit_p12/myapplication/MainActivity.kt
@@ -1,20 +1,30 @@
 package com.example.enpit_p12.myapplication
 
-import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.preference.PreferenceManager
 import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.startActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-    fun onJanKenButtonTapped(view: View?){
-        val intent = Intent(this, ResultActivity::class.java)
-        intent.putExtra("MY_HAND", )
-        startActivity(intent)
+
+        gu.setOnClickListener { onJanKenButtonTapped(it) }
+        choki.setOnClickListener { onJanKenButtonTapped(it) }
+        pa.setOnClickListener { onJanKenButtonTapped(it) }
+
+        val pref = PreferenceManager.getDefaultSharedPreferences(this)
+        val editor = pref.edit()
+        editor.clear().apply()
+
     }
 
+    fun onJanKenButtonTapped(view: View?){
+       startActivity<ResultActivity>("MY_HAND" to view?.id)
+    }
 }
+
